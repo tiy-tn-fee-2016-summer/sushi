d50580ce2dac107ec5353b1fd9596949c48472ed
Ryan Tablada <ryan.tablada@gmail.com>
2016-08-08 11:35:49 -0500

    Use didReceiveAttrs to figure out inline style

--- app/components/game-card/component.js
+++ app/components/game-card/component.js
@@ -1,5 +1,33 @@
 import Ember from 'ember';
 
+const sushiInfo = {
+  width: 10,
+  height: 7,
+  image: 'sushi.jpg',
+};
+
+const dominionInfo = {
+  width: 5,
+  height: 2,
+  image: 'dominion.jpg',
+}
+
 export default Ember.Component.extend({
   tagName: '',
+
+  didReceiveAttrs() {
+    if (this.get('game') === 'sushi') {
+      this.set('imagePath', sushiInfo.image);
+      this.computeStyle(sushiInfo);
+    } else {
+      this.set('imagePath', dominionInfo.image);
+      this.computeStyle(dominionInfo);
+    }
+  },
+
+  computeStyle(gameDetails) {
+    const widthStyle = `width: calc(100% * ${gameDetails.width});`;
+    const heightStyle = `height: calc(100% * ${gameDetails.height});`;
+    this.set('imageStyle', `${widthStyle} ${heightStyle}`);
+  },
 });
